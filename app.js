3766fa8d563a4c790b5b0a372537ffff9d55644a
Irvingjmp <irvingjmp@gmail.com>
2024-11-29 14:28:42 -0600

    Live coding 1

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// Escribe tu código aquí.
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+// alert("Palabras");
+function palabrasGrandes(palabra, arreglo){
+    return arreglo.filter(elemento => elemento.length > palabra.length);
+}
+
+const nuevaLista= palabrasGrandes('bocina', myArray);
+console.log(nuevaLista);
+
+function printArray(array){
+    const $ul =document.getElementById("listaPalabras");
+
+    array.forEach(elemento=>{
+        const $li = document.createElement("li");
+        $li.textContent = elemento;
+        $ul.appendChild($li);
+    });
+}
+
+printArray(myArray);
+
+
